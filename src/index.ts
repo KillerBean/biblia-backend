a527a663bbfb80f6807719c1e2127bd2d77eec27
Luis Roseno <nando.roseno@gmail.com>
2024-10-21 02:40:13 -0300

    carregar o arquivo .env

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express'
+import { configDotenv } from 'dotenv';
 
 import apiRouter from "./router";
 import swaggerFile from "./swagger-output.json"
 
-// Porta do servidor
-const PORT = process.env.PORT || 4000
+// carrega o arquivo .env
+configDotenv()
 
-// Host do servidor
+const PORT = process.env.PORT || 4000
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 
 // App Express
@@ -17,19 +18,19 @@ const app = express()
 
 /* Middlewares */
 app.use(bodyParser.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use('/api', apiRouter)
-
-// Endpoint raiz
-app.get('/', (req, res) => {
-    res.send('Bem-vindo!')
-})
-
 // Cors
 app.use(cors({
     origin: ['http://localhost:4000']
 }))
+// Docs swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// Rotas
+app.use('/api', apiRouter)
+app.get('/', (req, res) => {
+    res.send('Bem-vindo!')
+})
+
 
 // Resposta padrão para quaisquer outras requisições:
 app.use((req, res) => {

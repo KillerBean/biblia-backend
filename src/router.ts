df6b53b86623e61ac28754d0aa0612d864c0d1b6
Luis Roseno <nando.roseno@gmail.com>
2024-10-21 20:28:14 -0300

    importa o controller no arquivo de rotas

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import SourceController from './controllers/source-controller'
 const apiRouter = express.Router()
 
-apiRouter.get('/', (req, res) => {
-    res.send('Lê todos os itens')
+apiRouter.get('/', (req, res, next) => {
+    res.send(SourceController.index())
+})
+
+apiRouter.get('/versions', (req, res, next) => {
+    res.send(SourceController.getVersionList())
 })
 
 export default apiRouter

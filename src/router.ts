c1d2bdf0a1877299d46a155370f60e81a6ac526a
Luis Roseno <nando.roseno@gmail.com>
2024-11-28 12:46:24 -0300

    mais 1 endpoint

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,16 @@ apiRouter.get('/', (req, res, next) => {
     res.send(SourceController.index())
 })
 
+apiRouter.get('/books/:testamentId?', async (req, res, next) => {
+    let testamentId: number | undefined
+    if(req.params.testamentId){
+        testamentId = Number.parseInt(req.params.testamentId)
+    }
+    let result = await SourceController.getBooks(testamentId)
+    console.log(result)
+    res.json(result)
+})
+
 apiRouter.get('/versions', async (req, res, next) => {
     const versions = await SourceController.getVersionList()
     res.send(versions)

981c86c59961ffbe74513834aa083c1c9f1b1d2b
Luis Roseno <nando.roseno@gmail.com>
2024-12-03 10:19:41 -0300

    inclui informações de ajuda no launch script

--- launch.sh
+++ launch.sh
@@ -15,8 +15,18 @@ while i in "$@"; do
             docker compose up --build -d --force-recreate
             shift
             ;;
+        -h|--help)
+            echo "Usage: $0 [options]"
+            echo "Options:"
+            echo "  -d, --down    Stop and remove containers"
+            echo "  -u, --up      Build and start containers"
+            echo "  -a, --all     Stop and remove containers, then build and start containers"
+            echo "  -h, --help    Show this help message"
+            exit 0
+            ;;
         *)
             echo "Unknown option: $i"
+            echo "Use -h or --help for help"
             exit 1
             ;;
     esac
